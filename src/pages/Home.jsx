364d0017d0ee9dbe41870a4a41e03d7b977186de
C Bhuvaneshvar Reddy <bhuvaneshvar.r.c@gmail.com>
2025-04-06 19:40:29 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import home from "../assets/home.jpg"; 
+import { Link } from "react-router-dom";
 
 const Home = () => (
   <div
@@ -19,12 +20,14 @@ const Home = () => (
       <p className="text-lg mb-6 text-neutral-300 mt-2">
         Find the best gym equipment and supplements at unbeatable prices.
       </p>
+      <Link to="/products">
       <a
         href="/products"
         className="py-2 px-4 rounded-md bg-gradient-to-r from-orange-400 to-red-700 hover:from-orange-400 hover:to-red-600 transition"
       >
         Shop Now
       </a>
+        </Link>
     </div>
   </div>
 );
