ba6ba90220a1e89e05cf53d71329782601ff3374
C Bhuvaneshvar Reddy <bhuvaneshvar.r.c@gmail.com>
2025-04-06 17:26:08 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch("https://gymstore3-2.onrender.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
